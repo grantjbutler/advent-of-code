972e23f567e1c78ca82db00daa469504b167fb61
Grant Butler <grantjbutler@users.noreply.github.com>
2021-12-07 00:28:32 -0500

    Add solution for Day 7.

---
+++ src/AOC2021/Day7/Day7.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AOC2021\Day7;
+
+use AOC\Day;
+use AOC\Input;
+use Ds\Map;
+
+class Day7 extends Day {
+    public function part1(Input $input) {
+        $positions = $input->explode(',')
+            ->map(fn ($num) => (int)$num);
+
+        $idealPosition = $positions->median();
+        
+        return $positions->reduce(function ($total, $position) use ($idealPosition) {
+            return $total + abs($position - $idealPosition);
+        }, 0);
+    }
+
+    public function part2(Input $input) {
+        $positions = $input->explode(',')
+            ->map(fn ($num) => (int)$num);
+        
+        return collect()
+            ->range($positions->min(), $positions->max())
+            ->min(function ($position) use ($positions) {
+                return $positions->reduce(function($total, $crab) use ($position) {
+                    $distance = abs($position - $crab);
+                    return $total + ($distance * ($distance + 1)) / 2;
+                }, 0);
+            });
+    }
+}
