f083e4d524ca227296af274fc458ca1b106de4be
Grant Butler <grantjbutler@users.noreply.github.com>
2021-12-09 00:49:33 -0500

    Add solution for day 9.

---
+++ src/AOC2021/Day9/Day9.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AOC2021\Day9;
+
+use AOC\Day;
+use AOC\Input;
+
+class Day9 extends Day {
+    public function part1(Input $input) {
+        $map = $input->lines
+            ->flatMap(function($line) {
+                return $line->split(1)
+                    ->asIntegers();
+            });
+        
+        $width = $input->lines->first()->length();
+
+        return $map->filter(fn ($height, $key) => $this->adjacentIndicies($key, $width, $map->count())->every(fn ($index) => $map[$index] > $height))
+            ->map(fn ($height) => $height + 1)
+            ->sum();
+    }
+
+    public function part2(Input $input) {
+        $map = $input->lines
+            ->flatMap(function($line) {
+                return $line->split(1)
+                    ->asIntegers();
+            });
+        
+        $width = $input->lines->first()->length();
+        $self = $this;
+
+        $basins = $map->filter(fn ($height, $key) => $this->adjacentIndicies($key, $width, $map->count())->every(fn ($index) => $map[$index] > $height))
+            ->flatMap(function($height, $key) use ($self, $width, $map) {
+                $indiciesToCheck = collect([$key]);
+                $basinIndicies = collect([$key]);
+                
+                while ($indiciesToCheck->count()) {
+                    $index = $indiciesToCheck->shift();
+                    $height = $map[$index];
+                    
+                    $newIndicies = $self->adjacentIndicies($index, $width, $map->count())
+                        ->filter(fn ($value) => $map[$value] != 9 && $map[$value] >= $height && !$basinIndicies->contains($value));
+                    $basinIndicies = $basinIndicies->concat($newIndicies);
+                    
+                    $indiciesToCheck = $indiciesToCheck->concat($newIndicies);
+                }
+                
+                return $basinIndicies;
+            });
+        
+        $output = new \Symfony\Component\Console\Output\ConsoleOutput;
+        $map->map(function($value, $key) use ($basins) {
+            // var_dump($key);
+            // return $value;
+            if ($basins->contains(fn ($value) => $value == $key)) {
+                return '<options=bold>' . $value . '</>';
+            }
+            return '<fg=#777777>' . $value . '</>';
+        })
+        ->chunk($width)
+        ->map(fn($line) => $line->join(''))
+        ->each(fn ($line) => $output->writeln($line));
+
+
+        return $map->filter(fn ($height, $key) => $this->adjacentIndicies($key, $width, $map->count())->every(fn ($index) => $map[$index] > $height))
+            ->map(function($height, $key) use ($self, $width, $map) {
+                $indiciesToCheck = collect([$key]);
+                $basinIndicies = collect([$key]);
+                
+                while ($indiciesToCheck->count()) {
+                    $index = $indiciesToCheck->shift();
+                    $height = $map[$index];
+                    
+                    $newIndicies = $self->adjacentIndicies($index, $width, $map->count())
+                        ->filter(fn ($value) => $map[$value] != 9 && $map[$value] >= $height && !$basinIndicies->contains($value));
+                    $basinIndicies = $basinIndicies->concat($newIndicies);
+                    
+                    $indiciesToCheck = $indiciesToCheck->concat($newIndicies);
+                }
+                
+                return $basinIndicies->count();
+            })
+            ->sortDesc()
+            ->take(3)
+            ->reduce(function($total, $value) {
+                return $total * $value;
+            }, 1);
+    }
+
+    private function adjacentIndicies($key, $width, $length) {
+        return collect([$key - 1, $key + 1, $key - $width, $key + $width])
+            ->filter(fn ($index) => $index >= 0 && $index < $length);
+    }
+}
