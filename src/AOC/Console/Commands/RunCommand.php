7f5dc4fc5e43c49ffa33bb6ea64837df675051cd
Grant J. Butler <grantjbutler@users.noreply.github.com>
2022-12-06 00:21:44 -0500

    Throw exception if a part doesn't return a result.

--- src/AOC/Console/Commands/RunCommand.php
+++ src/AOC/Console/Commands/RunCommand.php
@@ -18,18 +18,9 @@ class RunCommand extends Command {
         
         $input = $this->loadInput($year, $day);
         $day = $this->createDay($year, $day);
-        
-        $start = microtime(true);
-        $this->info('Part 1: ' . $day->part1($input));
-        $end = microtime(true);
 
-        $this->info('Part 1 executed in ' . $end - $start . ' seconds');
-
-        $start = microtime(true);
-        $this->info('Part 2: ' . $day->part2($input));
-        $end = microtime(true);
-
-        $this->info('Part 2 executed in ' . $end - $start . ' seconds');
+        $this->runPart(1, $day, $input);
+        $this->runPart(2, $day, $input);
     }
 
     private function loadDay() {
@@ -52,5 +43,21 @@ class RunCommand extends Command {
         } else {
             return new Input($inputFolder . DIRECTORY_SEPARATOR . 'input.txt');
         }
+    }
+
+    private function runPart(int $part, Day $day, Input $input) {
+        $function = 'part' . $part;
+
+        $start = microtime(true);
+        $result = $day->{$function}($input);
+        $end = microtime(true);
+
+        if (is_null($result)) {
+            throw new \Exception("Part {$part} didn't return a result. Are you missing a return statement?");
+        }
+
+        $this->info("Part {$part}: {$result}");
+        $this->info("Part {$part} executed in ". $end - $start . " seconds");
+
     }
 }
