2c5c4811db8d373273e38d3a4962c013d0eaca71
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-12-06 00:19:10 -0500

    Add solution for day 6.

---
+++ swift/Sources/AOC2023/Day6.swift
@@ -0,0 +1,83 @@
+import AOCKit
+
+public enum Day6: Solution {
+    public typealias SolutionInput = String
+
+    public static func part1(_ input: SolutionInput) throws -> some CustomStringConvertible {
+        return try raceParser.parse(input[...])
+            .map { time, record in
+                return (1..<time).map { pressDuration in
+                    let distance = pressDuration * (time - pressDuration)
+                    return distance > record ? 1 : 0
+                }
+                .sum()
+            }
+            .product()
+    }
+    
+    public static func part2(_ input: SolutionInput) throws -> some CustomStringConvertible {
+        let (time, record) = try part2Parser.parse(input[...])
+        
+        return (1..<time).map { pressDuration in
+            let distance = pressDuration * (time - pressDuration)
+            return distance > record ? 1 : 0
+        }
+        .sum()
+    }
+}
+
+private let raceParser = Parse(input: Substring.self) { times, distances in
+    return zip(times, distances).map { (time: $0, distance: $1) }
+} with: {
+    "Time:"
+    Whitespace()
+    
+    Many {
+        Int.parser()
+    } separator: {
+        Whitespace()
+    }
+    
+    Whitespace(1, .vertical)
+    
+    "Distance:"
+    Whitespace()
+    
+    Many {
+        Int.parser()
+    } separator: {
+        Whitespace()
+    }
+}
+
+private let part2Parser = Parse(input: Substring.self) {
+    "Time:"
+    Whitespace()
+    
+    Many {
+        Int.parser()
+    } separator: {
+        Whitespace()
+    }
+    .map { ints in
+        Int(ints.reduce(into: "") { partialResult, int in
+            partialResult += "\(int)"
+        })!
+    }
+    
+    Whitespace(1, .vertical)
+    
+    "Distance:"
+    Whitespace()
+    
+    Many {
+        Int.parser()
+    } separator: {
+        Whitespace()
+    }
+    .map { ints in
+        Int(ints.reduce(into: "") { partialResult, int in
+            partialResult += "\(int)"
+        })!
+    }
+}
