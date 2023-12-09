2f677ca56595cb3f31bdc8bc4d1154a6c0fc7ab8
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-12-09 10:57:40 -0500

    Add solution for day 9.

---
+++ swift/Sources/AOC2023/Day9.swift
@@ -0,0 +1,71 @@
+import AOCKit
+
+public enum Day9: Solution {
+    public typealias SolutionInput = String
+
+    public static func part1(_ input: SolutionInput) throws -> some CustomStringConvertible {
+        return try input
+            .lines
+            .map { line in
+                let numbers = try integersParser.parse(line[...])
+                
+                var sequences: [[Int]] = [numbers]
+                var currentSequence = numbers
+                
+                repeat {
+                    let subtractionSequence = currentSequence
+                        .windows(ofCount: 2)
+                        .map { $0.last! - $0.first! }
+                    
+                    sequences.append(subtractionSequence)
+                    
+                    if subtractionSequence.allSatisfy({ $0 == .zero }) { break }
+                    currentSequence = subtractionSequence
+                } while true
+                
+                return sequences
+                    .reversed()
+                    .reduce(into: 0) { partialResult, sequence in
+                        partialResult = partialResult + sequence.last!
+                    }
+            }
+            .sum()
+    }
+    
+    public static func part2(_ input: SolutionInput) throws -> some CustomStringConvertible {
+        return try input
+            .lines
+            .map { line in
+                let numbers = try integersParser.parse(line[...])
+                
+                var sequences: [[Int]] = [numbers]
+                var currentSequence = numbers
+                
+                repeat {
+                    let subtractionSequence = currentSequence
+                        .windows(ofCount: 2)
+                        .map { $0.last! - $0.first! }
+                    
+                    sequences.append(subtractionSequence)
+                    
+                    if subtractionSequence.allSatisfy({ $0 == .zero }) { break }
+                    currentSequence = subtractionSequence
+                } while true
+                
+                return sequences
+                    .reversed()
+                    .reduce(into: 0) { partialResult, sequence in
+                        partialResult = sequence.first! - partialResult
+                    }
+            }
+            .sum()
+    }
+}
+
+private let integersParser = Parse(input: Substring.self) {
+    Many {
+        Int.parser()
+    } separator: {
+        Whitespace()
+    }
+}
