51f3aa48e4d850bde1431b94ad461e20e8dcd7bd
Grant J. Butler <grantjbutler@users.noreply.github.com>
2025-12-01 00:57:26 -0500

    Refine Day 1 solution a little.

--- swift/Sources/AOC2025/Day1.swift
+++ swift/Sources/AOC2025/Day1.swift
@@ -6,12 +6,7 @@ public enum Day1: Solution {
     public static func part1(_ input: SolutionInput) throws -> some CustomStringConvertible {
         let moves = try RotationParser().parse(input[...])
         let state = moves.reduce(into: (position: 50, count: 0)) { state, move in
-            switch move.direction {
-            case .right: state.position += move.amount
-            case .left: state.position -= move.amount
-            }
-            
-            state.position %= 100
+            state.position = move.direction(state.position, move.amount) % 100
             
             if state.position == 0 {
                 state.count += 1
@@ -24,12 +19,12 @@ public enum Day1: Solution {
     public static func part2(_ input: SolutionInput) throws -> some CustomStringConvertible {
         let moves = try RotationParser().parse(input[...])
         let state = moves.reduce(into: (position: 50, count: 0)) { state, move in
-            for value in stride(from: state.position, through: move.direction.operation(state.position, move.amount), by: move.direction.sign * 1).dropFirst()
+            for value in stride(from: state.position, through: move.direction(state.position, move.amount), by: move.direction.step).dropFirst()
                 where value % 100 == 0 {
                 state.count += 1
             }
             
-            state.position = move.direction.operation(state.position, move.amount)
+            state.position = move.direction(state.position, move.amount)
         }
         
         return state.count
@@ -37,9 +32,9 @@ public enum Day1: Solution {
 }
 
 private struct Move {
-    enum Direction {
-        case left
-        case right
+    enum Direction: String, CaseIterable {
+        case left = "L"
+        case right = "R"
         
         var operation: (Int, Int) -> Int {
             switch self {
@@ -48,11 +43,15 @@ private struct Move {
             }
         }
         
-        var sign: Int {
+        var step: Int {
             switch self {
             case .left: return -1
             case .right: return 1
             }
+        }
+        
+        func callAsFunction(_ a: Int, _ b: Int) -> Int {
+            operation(a, b)
         }
     }
     
@@ -66,10 +65,7 @@ private struct RotationParser: Parser {
             $0.map(Move.init)
         } with: {
             Many {
-                OneOf {
-                    "R".map(.case(Move.Direction.right))
-                    "L".map(.case(Move.Direction.left))
-                }
+                Move.Direction.parser()
                 
                 Int.parser()
             } separator: {
